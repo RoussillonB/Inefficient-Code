267a14b111fbc3a0a9d0e4f359a8fd63e5a0a901
Alexis Janon <kardyne@gmail.com>
2017-05-08 01:13:08 +0200

    Added JavaDoc for PermutationSort
    Added JavaDoc for the class and the two helper functions.

--- Sorting/Java/src/main/benvolio/inefficientcode/sort/PermutationSort.java
+++ Sorting/Java/src/main/benvolio/inefficientcode/sort/PermutationSort.java
@@ -4,6 +4,14 @@ import org.apache.commons.math3.util.CombinatoricsUtils;
 
 import benvolio.inefficientcode.sort.helper.ExtendedArrayInstCounter;
 
+/**
+ * Permutes the array until it is sorted.
+ * Based on the C++ std function next_permutation.
+ * 
+ * @author Kardyne
+ *
+ */
+
 public class PermutationSort extends InefficientSort {
 
 	@Override
@@ -12,6 +20,12 @@ public class PermutationSort extends InefficientSort {
 	}
 
 
+	/**
+	 * Changes the array to its next permutation. Based on the C++ function of
+	 * the same name.
+	 * @param array the array to permute
+	 * @return true if the array is sorted (side effect of the method)
+	 */
 	private boolean nextPermutation(ExtendedArrayInstCounter array) {
 		int first = 0;
 		int last = array.length();
@@ -42,7 +56,12 @@ public class PermutationSort extends InefficientSort {
 		}
 	}
 
-
+	/**
+	 * Reverses the array. Based on the C++ function of the same name.
+	 * @param array the array to reverse
+	 * @param first where to begin the reversal in the array
+	 * @param last where to end the reversal in the array
+	 */
 	private void reverse(ExtendedArrayInstCounter array, int first, int last) {
 		while ((first!=last)&&(first!=--last)) {
 			array.swap(first, last);
